efb5443c7537d3dead929d4fbf88844db94ddaff
gletchik <gleb_vgv@mail.ru>
2025-02-20 14:49:41 +0300

    create class Num Button

---
+++ NumButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace calculator
+{
+    [ToolboxItem(true)]
+    [Designer(typeof(ControlDesigner))]
+    internal class NumButton:Button 
+    {
+         // Делаем переменную num свойством
+        [Browsable(true)] // Отображать в свойствах
+        [Category("Appearance")] // Категория в свойствах
+        [Description("Число, связанное с кнопкой")] // Описание свойства
+        public string Num
+        {
+            get { return num; }
+            set { num = value; }
+        }
+
+        private string num;
+
+        public NumButton() : base()
+        {
+            this.num = null;
+        }
+
+        public NumButton(string num) : base()
+        {
+            this.num = num;
+        }
+
+        public void AddNum(TextBox textBox)
+        {
+            textBox.Text += num;
+        }
+    }
+}
